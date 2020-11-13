7a9d6a3c6950dd4f79f2e42fdee46667c4878802
Jonafran <stellwagjonathan@gmail.com>
2020-11-13 13:25:36 +0100

    2 files modified

--- 01-program/program.ts
+++ 01-program/program.ts
@@ -5,28 +5,32 @@ export namespace Program {
     args?: Array<string>
   ): fplibrary.Description<void> =>
     fplibrary.Description.create(() =>
-      display(
-        hyphens(
-          display(
-            createMessage(
-              round(
-                ensurePositiveAmount(
-                  convertStringToInteger(
-                    prompt(display(question(display(hyphens(args)))))
-                  )
-                )
-              )
-            )
+      fplibrary
+        .compose(
+          fplibrary.compose(
+            hyphens,
+            display,
+            question,
+            display,
+            prompt,
+            convertStringToInteger,
+            ensurePositiveAmount,
+            round,
+            createMessage
+          ),
+          fplibrary.compose(
+            display,
+            hyphens,
+            display
           )
         )
-      )
+        .apply(args)
     );
-
-  const hyphens = (arg?: any) => "\u2500".repeat(50);
-  const question = (arg?: any) => "How much do you want to deposit?";
+  const hyphens = (arg: any) => "\u2500".repeat(50);
+  const question = (arg: any) => "How much do you want to deposit?";
 
   const display = console.log;
-  const prompt = (arg?: any) => "5";
+  const prompt = (arg: any) => "5";
   const convertStringToInteger = Number;
   const ensurePositiveAmount = (amount: number) => Math.abs(amount) || 1;
   const round = (amount: number): number =>
